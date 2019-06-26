b0582996ecf979d9391cc83c881877142a683b5f
egl241 <egl241@163.com>
2019-06-26 10:49:26 +0800

    删除system.out

--- dri-cloud-common/dri-cloud-oauth/src/main/java/com/dri/config/AuthorizationServerConfig.java
+++ dri-cloud-common/dri-cloud-oauth/src/main/java/com/dri/config/AuthorizationServerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -14,7 +13,6 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
 * 授权服务器配置
@@ -42,7 +40,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
      */
 	@Bean
 	public TokenStore tokenStore() {
-		System.out.println("MyAuthorizationServerConfig tokenStore()");
 		//return new RedisTokenStore(redisConnectionFactory);
 		return new JwtTokenStore(jwtAccessTokenConverter);
 	}
