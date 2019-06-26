137cb4317fb5e025f6e96e4f6b14bb56ebb81050
zhaimingming <365719836@qq.com>
2019-06-26 10:43:58 +0800

    代码整理

--- dri-cloud-common/dri-cloud-gateway/src/main/java/com/dri/filter/RateLimiterFilter.java
+++ dri-cloud-common/dri-cloud-gateway/src/main/java/com/dri/filter/RateLimiterFilter.java
@@ -1,6 +1,8 @@
 package com.dri.filter;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +14,13 @@ import com.netflix.zuul.exception.ZuulException;
 
 public class RateLimiterFilter extends ZuulFilter{
 	
+	private static final Logger log = LoggerFactory.getLogger(RateLimiterFilter.class);
+	
 	private static final RateLimiter RATE_LIMITER=RateLimiter.create(50);
 
 	@Override
 	public boolean shouldFilter() {
-		// TODO Auto-generated method stub
+		
 		return true;
 	}
 
@@ -24,27 +28,25 @@ public class RateLimiterFilter extends ZuulFilter{
 	public Object run() throws ZuulException {
 		RequestContext requestContext = RequestContext.getCurrentContext();
 		
-		//就相当于每调用一次tryAcquire()方法，令牌数量减1，当1000个用完后，那么后面进来的用户无法访问上面接口
-        //当然这里只写类上面一个接口，可以这么写，实际可以在这里要加一层接口判断。
+		//每调用一次tryAcquire()方法，令牌数量减1，当1000个用完后，那么后面进来的用户无法访问上面接口
         if (!RATE_LIMITER.tryAcquire()) {
-        	System.out.println("=========RATE_LIMITER=======!!!!!!!!!!!!!!!");
+        	log.info("request be limited because of TOO_MANY_REQUESTS!");
             requestContext.setSendZuulResponse(false);
-            //HttpStatus.TOO_MANY_REQUESTS.value()里面有静态代码常量
             requestContext.setResponseStatusCode(HttpStatus.TOO_MANY_REQUESTS.value());
         }
-        System.out.println("=========RATE_LIMITER=======");
+        log.info("RateLimiterFilter run() method");
 		return null;
 	}
 
 	@Override
 	public String filterType() {
-		// TODO Auto-generated method stub
+		
 		return "pre";
 	}
 
 	@Override
 	public int filterOrder() {
-		// TODO Auto-generated method stub
+		
 		return -4;
 	}
 
