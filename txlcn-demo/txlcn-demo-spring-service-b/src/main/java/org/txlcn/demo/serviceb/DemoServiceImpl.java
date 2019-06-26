0d66f9219af99cd6503f57c62c448a6e5b013782
egl241 <egl241@163.com>
2019-06-26 17:50:54 +0800

    添加txlcn及demo

---
+++ txlcn-demo/txlcn-demo-spring-service-b/src/main/java/org/txlcn/demo/serviceb/DemoServiceImpl.java
@@ -0,0 +1,52 @@
+package org.txlcn.demo.serviceb;
+
+import com.codingapi.txlcn.common.util.Transactions;
+import com.codingapi.txlcn.tc.annotation.DTXPropagation;
+import com.codingapi.txlcn.tc.annotation.TxcTransaction;
+import com.codingapi.txlcn.tracing.TracingContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.txlcn.demo.common.db.domain.Demo;
+
+import java.util.Date;
+
+/**
+ * Description:
+ * Date: 2018/12/25
+ *
+ * @author ujued
+ */
+@Service
+@Slf4j
+public class DemoServiceImpl implements DemoService {
+
+    private final DemoMapper demoMapper;
+
+    @Autowired
+    public DemoServiceImpl(DemoMapper demoMapper) {
+        this.demoMapper = demoMapper;
+    }
+
+    @Override
+    @TxcTransaction(propagation = DTXPropagation.SUPPORTS)
+    @Transactional
+    public String rpc(String value) {
+
+
+        Demo demo = new Demo();
+        demo.setGroupId(TracingContext.tracing().groupId());
+        demo.setDemoField(value);
+        demo.setAppName(Transactions.getApplicationId());
+        demo.setCreateTime(new Date());
+        demoMapper.save(demo);
+        
+        // 置异常标志，DTX 回滚
+        if ("errorB".equals(value)) {
+            throw new IllegalStateException("by errorA");
+        }
+        
+        return "ok-service-b";
+    }
+}
