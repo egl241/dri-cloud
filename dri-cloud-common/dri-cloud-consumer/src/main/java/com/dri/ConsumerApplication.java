0b4a1bab995b6518a77e230ad82ba34b4c6a809e
zhaimingming <365719836@qq.com>
2019-06-27 11:36:06 +0800

    集成持久层mybatis

--- dri-cloud-common/dri-cloud-consumer/src/main/java/com/dri/ConsumerApplication.java
+++ dri-cloud-common/dri-cloud-consumer/src/main/java/com/dri/ConsumerApplication.java
@@ -1,5 +1,6 @@
 package com.dri;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.dri.mapper")
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
---
+++ dri-cloud-common/dri-cloud-consumer/src/main/java/com/dri/mapper/AccountMapper.java
@@ -0,0 +1,8 @@
+package com.dri.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dri.entity.Account;
+
+public interface AccountMapper extends BaseMapper<Account>{
+
+}
